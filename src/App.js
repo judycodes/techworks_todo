2c067b7d5d2be3397bf748306990a9280429371d
Judy <codelinked@gmail.com>
2019-07-15 17:41:21 -0400

    todo beginning

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ToDoList extends Component {
+  constructor(){
+    super();
+    this.state = {
+      todos: ["Wake up", "Take a shower", "Drive to walk", "Teach", "Git commit all code"],
+      currentToDo: ""
+    }
+  }
+
+  handleChange = event => {
+    console.log(event.target.value); //this shows the input values as it is typed into the input text box
+    this.setState({
+      currentToDo : event.target.value
+    })
+  }
+
+  addItem = event => {
+    event.preventDefault(); //stop propogation
+    //refresh/rerendering is the default action 
+    //console.log("addItem fires!") //shows in console the addItem method being fired from the submission of form data
+    this.setState({
+      todos: this.state.todos.push(this.state.currentToDo)
+    })
+    
+    console.log(this.state.todos, 'all todos');
+    console.log(this.state.currentToDo, 'current to do')
+  }
+
+  
+  render(){
+    return (
+      <div className = "container">
+        <form onSubmit = {this.addItem}>
+          <label htmlFor = "taskName">Task Name: </label>
+          <input onChange = {this.handleChange} name = "taskName" type = "text" placeholder = " Add Your Next Task Here!" />
+          <button type = "submit">Add Task</button>
+        </form>
+
+        <ul className = "list">
+          {this.state.todos.map((todo) => <li>{todo}</li> )}
+        </ul>
+      </div>
+    )
+  };
 }
 
-export default App;
+export default ToDoList;
