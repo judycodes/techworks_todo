d24d4dbcf4829a0cc5b6349ee48c42b408e4da45
Judy <codelinked@gmail.com>
2019-07-21 14:04:48 -0400

    Updates

---
+++ precache-manifest.b40518cbd08c6606dd05966e02c37d97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05ce26f18d453d684b7692f8fd8d726b",
+    "url": "/techworks_todo/index.html"
+  },
+  {
+    "revision": "e02435970479159fd1b1",
+    "url": "/techworks_todo/static/css/main.04c9e9bc.chunk.css"
+  },
+  {
+    "revision": "faa34d332425c18142f5",
+    "url": "/techworks_todo/static/js/2.05fa6133.chunk.js"
+  },
+  {
+    "revision": "e02435970479159fd1b1",
+    "url": "/techworks_todo/static/js/main.10ca2683.chunk.js"
+  },
+  {
+    "revision": "6138b377dfc0a0766cb5",
+    "url": "/techworks_todo/static/js/runtime~main.7bd8007a.js"
+  },
+  {
+    "revision": "062f3f4a0ee7e7ef32cb7124ce110ddb",
+    "url": "/techworks_todo/static/media/nightdesk.062f3f4a.jpg"
+  }
+]);
